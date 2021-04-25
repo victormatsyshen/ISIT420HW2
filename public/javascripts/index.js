d0ab53c97fac2d9cf5daf940d2ae0f1575752d43
victormatsyshen <victormatsyshen@gmail.com>
2021-04-24 20:36:35 -0700

    Fix 1

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -12,25 +12,44 @@ function Music(pStoreID, pSalesPersonID, pCdID, pPricePaid, pHourPurch, pDayPurc
   // our local copy of the cloud data
   var ClientNotes = [];  
 
-  
 
-// Create 1 Button
-function CreateVal() {
-    var tStoreID = document.getElementById("StoreID").value;
-    var tSalesPersonID = document.getElementById("SalesPersonID").value;
-    var tCdID = document.getElementById("CdID").value;
-    var tPricePaid = document.getElementById("PricePaid").value;
-    var oneCD = new Music(tStoreID, tSalesPersonID, tCdID, tPricePaid);
-    //possibly add the hourPurch and dayPurch here too
+    // Submit 1 Button
+    function Submit1() {
+        var tStoreID = document.getElementById("StoreID").value;
+        var tSalesPersonID = document.getElementById("SalesPersonID").value;
+        var tCdID = document.getElementById("CdID").value;
+        var tPricePaid = document.getElementById("PricePaid").value;
+        var tHourPurch = GetHour();
+        var tDayPurch = GetDay();
+        //possibly add the hourPurch and dayPurch here too
+        
+        var oneMusic = new Music(tStoreID, tSalesPersonID, tCdID, tPricePaid, tHourPurch, tDayPurch);
+        console.log(oneMusic);
 
-    $.ajax({
-        url: '/NewCD' ,
-        method: 'POST',
-        dataType: 'json',
-        contentType: 'application/json',
-        data: JSON.stringify(oneCD),
-        success: function (result) {
-            console.log("added new music data!")
+        // Fill out
+        function GetHour()
+        {
+            return 1;
         }
-    });
-}
+
+        // Fill out
+        function GetDay()
+        {
+            return 1;
+        }
+
+        $.ajax({
+            url: '/NewMusic' ,
+            method: 'POST',
+            dataType: 'json',
+            contentType: 'application/json',
+            data: JSON.stringify(oneMusic),
+            success: function (result) {
+                console.log("added new music data!")
+            }
+        });
+    }
+
+    
+
+
